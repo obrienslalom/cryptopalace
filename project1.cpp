20312b8c9f62c479c82ee0587fb811c715fe85ce
root <root@localhost.localdomain>
2017-11-11 20:34:10 -0500

    snapshot (not working)

--- project1.cpp
+++ project1.cpp
@@ -2,6 +2,7 @@
 #include <cstring>
 #include <stdlib.h>
 #include <iostream>
+#include <math.h>
 
 const char* BASE64_TABLE = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
 
@@ -28,14 +29,23 @@ const char* HexToBase64(const char* c)
 
     unsigned sz = strlen(c);
     unsigned num_bits = sz * 4;
-    unsigned base64_size = num_bits / 6; 
-    char* const buff = ((char*) malloc(sizeof(char) * base64_size));
-    for(unsigned i = 0, j = 0; j < sz; i+=3, j+=2)
+    unsigned base64_size = (unsigned)ceil(num_bits / 6.0);
+    // initialize the buffer, so that the unaligned portions are zero'd out
+    char* const buff = ((char*) calloc(sizeof(char) * base64_size + 1, 1));
+    unsigned i = 0, j = 0, cnt = 0;
+    for(; i < sz - 2; i+=3, j+=2)
     {
         unsigned six_bit_val = (HexToByte(c[i]) << 2) + (HexToByte(c[i + 1]) >> 2); 
         buff[j] = BASE64_TABLE[six_bit_val];
         six_bit_val = ((HexToByte(c[i + 1]) & 0x3) << 4) + (HexToByte(c[i + 2]));
         buff[j+1] = BASE64_TABLE[six_bit_val];
+        cnt += 12;
+    }
+
+    if(cnt < num_bits)
+    {
+        unsigned v = (HexToByte(c[sz - 2]) << 2) + (HexToByte(c[sz -1]) >> 2);
+        buff[base64_size - 1] = BASE64_TABLE[v];
     }
     return buff;
 }
--- test_project1.cpp
+++ test_project1.cpp
@@ -1,3 +1,5 @@
+#define GTEST_USE_OWN_TR1_TUPLE 1
+
 #include <iostream>
 #include "gtest/gtest.h"
 
@@ -29,12 +31,18 @@ TEST(HexToBase64, HexToBase64Stage1Challenge1) {
         "SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t",
         HexToBase64("49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d")
     );
+    ASSERT_STREQ(
+        "SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb28=",
+        HexToBase64("49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f")
+    );
 }
 
+
+/*
 TEST(XOR, XORStage1Challenge2) {
     ASSERT_STREQ(
         "746865206b696420646f6e277420706c6179",
         XOR("1c0111001f010100061a024b53535009181c", "686974207468652062756c6c277320657965")
     );
 }
-
+*/
